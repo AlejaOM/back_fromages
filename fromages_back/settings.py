c2b611ff2d183cbc230843a54f971ee3429565a4
AlejaOM <ortizalejamedina@gmail.com>
2024-11-15 16:54:59 -0500

    allow host lh//4200

--- fromages_back/settings.py
+++ fromages_back/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key') #Render
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "http://localhost:4200",
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
