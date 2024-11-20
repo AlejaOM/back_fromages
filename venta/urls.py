8379914bbc1040e06da6b564679a078616bbe55e
nery2202021@correo.uis.edu.co <Nery Aponte>
2024-11-20 15:35:19 -0500

    se crearon las vistas para restar y agregar el stock al momento de agregar un producto, tambie esta la vista para la busqueda de productos

--- venta/urls.py
+++ venta/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IniciarVentaView, ClienteView, ProductoView
+from .views import IniciarVentaView, ClienteView, ProductoView, APaF, EPaF, ProductoList
 
 urlpatterns = [
     path('clientes/', ClienteView.as_view(), name='clientes'),
     path('productos/', ProductoView.as_view(), name='productos'),
+    path('ListaProductos/', ProductoList.as_view(), name='lista-productos'),
     path('iniciar-venta/', IniciarVentaView.as_view(), name='iniciar-venta'),
+    path('APaF/<int:id_producto>/', APaF.as_view(), name='agregar-producto-factura'),
+    path('EPaF/<int:id_producto>/', EPaF.as_view(), name='eliminar-producto-factura'),
 ]
